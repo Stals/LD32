3ce3351ef406922741ab98c9adc851659ef2542c
Stanislav Korotaev <superstals@gmail.com>
2015-04-19 17:08:56 +0300

    fake requirement

--- proj/Assets/ClientController.cs
+++ proj/Assets/ClientController.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public class Client{
+
+}
+
 public class ClientController : MonoBehaviour {
 
     public UI2DSprite avatar;
@@ -14,11 +18,21 @@ public class ClientController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        addRequirenment(new Requirenment(ResourceType.Wood, 2));
+        addRequirenment(new Requirenment(ResourceType.Metal, 3));
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void addRequirenment(Requirenment r){
+        GameObject req = NGUITools.AddChild(requirenmentsGrid.gameObject, requirenmentPrefab);
+        //requirenmentsGrid. //mb add child
+        
+        var requirenment = req.GetComponent<RequirenmentController>();
+        requirenment.create(r);
+        requirenmentsGrid.Reposition();
+    }
 }
--- proj/Assets/RequirenmentController.cs
+++ proj/Assets/RequirenmentController.cs
@@ -1,15 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum ResourceType{
+    Metal,
+    Gold,
+    Magic,
+    Wood,
+    Gem
+}
+
+public class Requirenment{
+    public ResourceType type;
+    public int amount;
+
+    public Requirenment(ResourceType _type, int _amount){
+        type = _type;
+        amount = _amount;
+    }
+
+};
+
 public class RequirenmentController : MonoBehaviour {
+
+    [SerializeField]
+    UILabel amountLabel;
+
+    Requirenment requirenment;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
+
+    public void create(Requirenment req){
+        requirenment = req;
+
+        amountLabel.text = requirenment.amount.ToString();
+        GetComponent<UISprite>().spriteName = typeToSprite(req.type);
+        GetComponent<UISprite>().MarkAsChanged();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    static string typeToSprite(ResourceType type){
+        switch (type)
+        {
+            case ResourceType.Metal: return "metal";
+            case ResourceType.Gold: return "gold";
+            case ResourceType.Magic: return "magic";
+            case ResourceType.Wood: return "wood";
+            case ResourceType.Gem: return "gem";
+            default: return "";
+        }
+
+    }
 }
