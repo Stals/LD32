9fe06c94a7231b235cd5d985a311eb36cb7812e9
Stanislav Korotaev <superstals@gmail.com>
2015-04-19 13:26:32 +0300

    max

--- proj/Assets/ResourceManager.cs
+++ proj/Assets/ResourceManager.cs
@@ -6,7 +6,14 @@ public class ResourceManager : MonoBehaviour {
 	[SerializeField]
 	UILabel amountLabel;
 
+    [SerializeField]
+    Color labelColorNormal;
+
+    [SerializeField]
+    Color labelColorMax;
+
 	int amount = 0;
+	int max = 10;
 
 	// Use this for initialization
 	void Start () {
@@ -16,10 +23,20 @@ public class ResourceManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		amountLabel.text = amount.ToString ();
+
+        if (amount == max)
+        {
+            amountLabel.color = labelColorMax;
+        } else
+        {
+            amountLabel.color = labelColorNormal;
+        }
 	}
 
 	void addBlock()
 	{
-		amount += 1;
+		if (amount < max) {
+			amount += 1;
+		}
 	}
 }
