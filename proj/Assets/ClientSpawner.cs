cfc3f472363453b8f0fc7c7764bbd0c65cc134ed
Stals <superstals@gmail.com>
2015-04-21 00:56:02 +0300

    tools for balancing (mb working)

--- proj/Assets/ClientSpawner.cs
+++ proj/Assets/ClientSpawner.cs
@@ -2,33 +2,145 @@
 using System.Collections;
 using System.Collections.Generic;
 
+
+public class Day {
+    public Day()
+    {
+        clients = new List<Client>();
+    }
+
+    public void Add(Client client) {
+        clients.Add(client);
+    }
+
+    public List<Client> clients;
+    public int clientsPerDay;
+
+    public bool isClientsRemaining()
+    {
+        return clientsPerDay != 0;
+    }
+}
+
+
+// когда дни кончились - переигрываем последний
+
+
 //TODO list of lists, from which we generate random stuff set number of times
 public class ClientSpawner : MonoBehaviour {
 
     [SerializeField]
     GameObject clientPanelPrefab;
 
-    List<Client> clients;
+    List<Day> days;
+
+    int currentDay = 0;
 
 	// Use this for initialization
-	void Start () {
-        clients = new List<Client>();
+    void Start()
+    {
+        days = new List<Day>();
 
-
+        // ========== DAY START =============
         {
-            Client client = new Client(getHeroName(), "", HeatType.Low);
-            client.addRequirenment(new Requirenment(ResourceType.Wood, Random.Range(1, 3)));
-            clients.Add(client);
-        }
+            Day day = new Day();
+            days.Add(day);
 
-        {
-            Client client = new Client(getHeroName(), "", HeatType.Low);
-            client.addRequirenment(new Requirenment(ResourceType.Metal, Random.Range(1, 3)));
-            clients.Add(client);
+            day.clientsPerDay = 3;
+            {
+                Client client = new Client(getHeroName(), "", HeatType.Low);
+                client.addRequirenment(new Requirenment(ResourceType.Wood, Random.Range(1, 3)));
+                day.Add(client);
+            }
+
+            {
+                Client client = new Client(getHeroName(), "", HeatType.Low);
+                client.addRequirenment(new Requirenment(ResourceType.Metal, Random.Range(1, 3)));
+                day.Add(client);
+            }
         }
-           
-        InvokeRepeating("spawn", 2f, 9f);	
-	}
+        // =======================
+
+        // ========== DAY START =============
+        {
+            Day day = new Day();
+            days.Add(day);
+
+            day.clientsPerDay = 5;
+            {
+                Client client = new Client(getHeroName(), "", HeatType.Low);
+                client.addRequirenment(new Requirenment(ResourceType.Wood, Random.Range(2, 3)));
+                client.addRequirenment(new Requirenment(ResourceType.Gold, Random.Range(2, 3)));
+                day.Add(client);
+            }
+
+            {
+                Client client = new Client(getHeroName(), "", HeatType.Low);
+                client.addRequirenment(new Requirenment(ResourceType.Magic, Random.Range(2, 3)));
+                day.Add(client);
+            }
+
+            {
+                Client client = new Client(getHeroName(), "", HeatType.Low);
+                client.addRequirenment(new Requirenment(ResourceType.Gem, Random.Range(2, 3)));
+                client.addRequirenment(new Requirenment(ResourceType.Wood, Random.Range(2, 3)));
+                day.Add(client);
+            }
+        }
+        // =======================
+
+        // ========== DAY START =============
+        // обязательный medium типок
+        {
+            Day day = new Day();
+            days.Add(day);
+
+            day.clientsPerDay = 1; 
+            {
+                Client client = new Client(getHeroName(), "", HeatType.Medium);
+                client.addRequirenment(new Requirenment(ResourceType.Metal, Random.Range(2, 3)));
+                day.Add(client);
+            }
+            
+        }
+        // =======================
+
+        // ========== DAY START =============
+        {
+            Day day = new Day();
+            days.Add(day);
+
+            day.clientsPerDay = 5;
+            {
+                Client client = new Client(getHeroName(), "", HeatType.Medium);
+                client.addRequirenment(new Requirenment(ResourceType.Wood, Random.Range(2, 3)));
+                client.addRequirenment(new Requirenment(ResourceType.Gold, Random.Range(2, 3)));
+                day.Add(client);
+            }
+
+            {
+                Client client = new Client(getHeroName(), "", HeatType.Medium);
+                client.addRequirenment(new Requirenment(ResourceType.Magic, Random.Range(2, 3)));
+                client.addRequirenment(new Requirenment(ResourceType.Gold, Random.Range(2, 3)));
+                day.Add(client);
+            }
+
+            {
+                Client client = new Client(getHeroName(), "", HeatType.Low);
+                client.addRequirenment(new Requirenment(ResourceType.Gem, Random.Range(2, 3)));
+                client.addRequirenment(new Requirenment(ResourceType.Wood, Random.Range(2, 3)));
+                day.Add(client);
+            }
+        }
+        // =======================
+
+        // TODO день для расслабления
+
+
+        // ВАЖНО для последнего дня задать day.clientsPerDay = -1;
+
+        InvokeRepeating("spawn", 2f, 9f);
+    }
 
     string getHeroName()
     {
@@ -47,6 +159,12 @@ public class ClientSpawner : MonoBehaviour {
         foreach (ClientSpace space in spaces)
         {
             if(space.empty){
+
+                if (!getCurrentDay().isClientsRemaining()) {
+                    currentDay += 1;
+                }
+
+
                 GameObject go = NGUITools.AddChild(space.gameObject, clientPanelPrefab);
 
                 ClientController clientController = go.GetComponent<ClientController>();
@@ -63,7 +181,17 @@ public class ClientSpawner : MonoBehaviour {
 
     Client getRandomClient()
     {
-        int rnd = Random.Range(0, clients.Count);
-        return clients[rnd];
+        Day day = getCurrentDay();
+
+        int rnd = Random.Range(0, day.clients.Count);
+        return day.clients[rnd];
     }
+
+    Day getCurrentDay()
+    {
+        return days[currentDay];
+
+        
+    }
+
 }
