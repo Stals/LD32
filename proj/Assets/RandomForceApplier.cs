23fe101d52064ba03a84a66fc31eaf8b3f6ee9a0
Stals <superstals@gmail.com>
2015-05-13 00:55:46 +0300

    glow button update

---
+++ proj/Assets/RandomForceApplier.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomForceApplier : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().AddForce(new Vector2 (Random.Range(-40f, 40f), 50f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
